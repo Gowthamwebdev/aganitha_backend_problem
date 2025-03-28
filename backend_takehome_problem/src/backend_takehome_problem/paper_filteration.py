09232b754d9b6817eb9a7c657764681919bdf1fb
gowtham <gowthamgalaxy007@gmail.com>
2025-03-28 23:33:16 +0530

    refactor: add paper_filteration

---
+++ backend_takehome_problem/src/backend_takehome_problem/paper_filteration.py
@@ -0,0 +1,3 @@
+# Keywords to classify affiliations
+ACADEMIC_KEYWORDS = {"university", "college", "institute", "hospital", "academy"}
+COMPANY_KEYWORDS = {"inc.", "ltd.", "corporation", "pharma", "biotech", "gmbh", "s.a."}
