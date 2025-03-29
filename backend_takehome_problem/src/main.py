46b493bb823bd340be1a6c0c4e02267240c1a3e2
gowtham <gowthamgalaxy007@gmail.com>
2025-03-30 00:11:49 +0530

    refactor: add cli comments

--- backend_takehome_problem/src/main.py
+++ backend_takehome_problem/src/main.py
@@ -10,9 +10,10 @@ from backend_takehome_problem.utils import save_to_csv
 from backend_takehome_problem.paper_filteration import filter_papers 
 
 def main():
-    parser = argparse.ArgumentParser(description="Fetch and filter research papers from PubMed.")
-    parser.add_argument("query", nargs="+", help="Search query (use quotes for multi-word queries)")
-    parser.add_argument("-f", "--file", type=str, help="Output CSV filename", default="papers.csv")
+    parser = argparse.ArgumentParser(description="Fetch research papers from PubMed based on a query.")
+    parser.add_argument("query", type=str, help="Search query for PubMed")
+    parser.add_argument("-d", "--debug", action="store_true", help="Enable debug mode to print additional information")
+    parser.add_argument("-f", "--file", type=str, help="Specify filename to save results. If not provided, output will be printed to console")
     args = parser.parse_args()
 
     query = " ".join(args.query).strip()
